bbabe5b47554a99ce562a00fb20485404525cfb5
Nguyen Vo Huu Tri <nguyenvohuutri@Nguyens-MacBook-Air.local>
2025-07-16 15:40:18 +0700

    fix: update alias @ to use path.resolve

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src", // Ánh xạ @/ đến src/
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {
